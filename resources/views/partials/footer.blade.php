30b98ce944dbea6466d00458075319364e7cc245
Manuel <manuel.mdz.rom@gmail.com>
2020-09-02 22:02:48 -0500

    Links
    Add change

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -4,17 +4,22 @@
      <input type="checkbox" id="btn-mas">
         <div class="redes">
             <a id="phone-big" onclick="gtag('event', 'click', {'event_category': 'telefono','event_label': 'llamada'});" ><i class="icon-mas2 fa fa-phone"></i></a>    
-            <a href="https://www.linkedin.com/"><i class="icon-mas2 fa fa-linkedin"></i></a>
+            <a href="https://www.linkedin.com/company/jap-engineering"><i class="icon-mas2 fa fa-linkedin"></i></a>
             <!-- <a href="https://www.facebook.com/japengineering20/"><i class="icon-mas2 fab fa-facebook-messenger"></i></a> -->
             <a href="https://www.messenger.com/t/japengineering20/"><i class="icon-mas2 fab fa-facebook-messenger"></i></a>
-            <!-- <a href="https://www.whatsapp.com/"><i class="icon-mas2 fa fa-whatsapp"></i></a> -->
+            <a href="https://api.whatsapp.com/send?phone=524491151133&fbclid=IwAR2mV8VZ7oZpqxY_Q5YsAtJC8S2bAF_5SXJlS5xPHbUGSoS-9RhQN60Rdbc"><i class="icon-mas2 fa fa-whatsapp"></i></a>
         </div>
         <div class="btn-mas">
             <label for="btn-mas" class="icon-mas2"><i class="fa fa-plus"></i></label>
         </div>
     </div>
-<p>
-<script> 
-</script>
-</p>
+
+    <!-- <div class="fb-customerchat"
+            attribution=setup_tool
+            page_id="108061834361813"
+            theme_color="#103446"
+            logged_in_greeting="Hola! Gracias por interesarte en nuestros servicios."
+            logged_out_greeting="Hola! Gracias por interesarte en nuestros servicios.">
+    </div> -->
+
 </footer>
